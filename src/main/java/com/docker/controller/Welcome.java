83642a724edd9499f2093af3e12c4e86e755c85c
Gowtham <krgowtham09122000@gmail.com>
2022-07-29 11:50:39 +0530

    code

---
+++ src/main/java/com/docker/controller/Welcome.java
@@ -0,0 +1,20 @@
+package com.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to Docker!";
+    }
+}
+
+//    git init
+//    git add README.md
+//        git commit -m "first commit"
+//        git branch -M main
+//        git remote add origin https://github.com/gowtham2509/Docker.git
+//        git push -u origin main
