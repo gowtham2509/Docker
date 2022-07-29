0fe54f7695dd46cde5444dc3ee453089f1e01a3c
Gowtham <krgowtham09122000@gmail.com>
2022-07-29 14:05:35 +0530

    added docker configure

--- src/main/java/com/docker/controller/Welcome.java
+++ src/main/java/com/docker/controller/Welcome.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Welcome {
 
-    @GetMapping
+    @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to Docker!";
     }
